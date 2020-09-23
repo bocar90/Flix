efb252926d97aee4575a94b8aa540e22359bf208
Bocar Sock <bocarsock5@gmail.com>
2020-09-23 13:11:16 -0400

    Flix Part 2

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,15 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Bocar Sock on 9/21/20.
+//  Copyright © 2020 bocarsock5@gmail.com. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+   
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
